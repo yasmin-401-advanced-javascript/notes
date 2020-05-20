8ab9bdef3b69351afdfadd091da391c2e3884322
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-05-20 04:52:41 +0300

    mongoDB working locally

--- notes.js
+++ notes.js
@@ -1,10 +1,37 @@
 'use strict';
-
+require('dotenv').config();
 // const Input = require('./lib/input.js');
-const Notes = require('./lib/notes.js');
+const Notess = require('./lib/notes.js');
+// const schema =require('./lib/model/notes-collection.js')
 
+const mongoose = require('mongoose');
+const Notes = require('./lib/model/notes-schema.js');
+const MONGOO_URI = 'mongodb://localhost:27017/notes';
+// const MONGOO_URI = process.env.MONGODB_URL;
+mongoose.connect(MONGOO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const doDataStuff = async () => {
+  let noteItem = {
+    Category: 'school',
+    addMessege: 'alben y6osha',
+  };
+  let note = new Notes(noteItem);
+  
+  await note.save();
+  console.log('this is ', note);
+  let oneNote = await Notes.findById(note.id);
+  console.log('this is id ', oneNote);
+  let allNote = await Notes.find({});
+  console.log('this is all ', allNote);
+  mongoose.disconnect();
+  
+};
 // const trying = new Input();
 
-const data = new Notes();
+const data = new Notess();
 data.execute();
 // data.add();
+doDataStuff();
