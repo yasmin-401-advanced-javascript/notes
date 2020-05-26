83de4d7132ec7a165af36e85b9eccc2ca8ec9745
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-05-26 21:26:30 +0300

    add validation

--- notes.js
+++ notes.js
@@ -1,10 +1,11 @@
 'use strict';
-require('dotenv').config();
+
+
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
-
 const trying = new Input();
 const data = new Notes(trying);
-console.log(data);
-
+data.execute(trying);
+// console.log(data);
 
